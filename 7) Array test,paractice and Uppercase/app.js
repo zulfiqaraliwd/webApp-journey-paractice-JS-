e0c5aa6b16306e6fb56ee6cfb150abad34ae6d54
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2025-12-15 19:00:04 +0500

    new folder added Function in js

--- 7) Array test,paractice and Uppercase/app.js
+++ 7) Array test,paractice and Uppercase/app.js
@@ -113,19 +113,19 @@ console.log("hellow");
 // use break in loops
 //----------------------
 
-var cityToCheck = prompt("enter the city name")
-var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu","skardu"];
-var matchFound = false;
-for(i = 0;i<cleanestCities.length;i++)
-  if(cityToCheck === cleanestCities[i]){
-    matchFound = true ;{
-alert("it is one of the cleanest city")
-break;
-    }
-  }
-  if(matchFound === false){
-    alert("it is not found")
-  }
+// var cityToCheck = prompt("enter the city name")
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu","skardu"];
+// var matchFound = false;
+// for(i = 0;i<cleanestCities.length;i++)
+//   if(cityToCheck === cleanestCities[i]){
+//     matchFound = true ;{
+// alert("it is one of the cleanest city")
+// break;
+//     }
+//   }
+//   if(matchFound === false){
+//     alert("it is not found")
+//   }
 
 
 //----------------------------------------------------------------------------------------
