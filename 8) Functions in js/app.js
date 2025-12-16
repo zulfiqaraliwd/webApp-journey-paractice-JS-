a1e73f117649f083e1e40b66c979784b32afb24a
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2025-12-16 23:44:20 +0500

    Removed submodule reference

---
+++ 8) Functions in js/app.js
@@ -0,0 +1,84 @@
+console.log("functions")
+
+
+// function myFunction(){
+//     console.log("inside the function")
+// }
+// myFunction()   //use "invoke" word instead of "call function"
+// myFunction()
+// myFunction()
+
+
+//arrow functions 
+// myFunction =() =>console.log("arrow function inside")
+
+// myFunction()
+// myFunction()
+// myFunction()
+
+
+//  function func(lName,fName) { //Parameter
+ 
+//   console.log(fName,lName);
+// }
+// var fName = "ibrahim";
+// func("khan",fName); //Argument
+
+
+// function func(fName,lName) { //Parameter
+ 
+//   console.log(fName,lName);
+// }
+// var fName = "ibrahim";
+// func("khan",fName); //Argument
+
+
+// function func(fName,lName) { //Parameter
+ 
+//   console.log(fName,lName);
+// }
+// var fName = "ibrahim";
+// func(fName,"khan"); //Argument
+
+
+
+//a function which is adding two numbers
+
+// function add(a,b){
+//     console.log(a+b)
+// }
+
+// add(4,99)
+
+
+
+//using alert inside a function
+
+
+// var num = 123;
+// function alertfunc(){
+//     alert(num)
+
+// }
+
+// alertfunc()
+
+
+// function with parameters and passing arguments
+
+// function paraArgu(a,b){
+// console.log(console.log(b-a))
+// }
+
+// paraArgu(88,66)
+
+
+
+//in functions we can store the parameters in variable , at a same time direct parameter also can be used.
+
+// function paragraph(name ,age,roll){
+//     console.log(name,age,roll)
+// }
+// var name = "asad"
+// var roll = 10
+// paragraph(name,roll,123)
