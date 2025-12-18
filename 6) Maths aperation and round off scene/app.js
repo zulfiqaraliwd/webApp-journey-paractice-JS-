8a00cf00ff9c92fbb16c530901d3a25cebf5a6e0
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2025-12-18 18:35:38 +0500

    round off and math's method in js

---
+++ 6) Maths aperation and round off scene/app.js
@@ -0,0 +1,21 @@
+console.log("round off the numbers in js")
+
+
+// var score  = 2.7
+// var mathematics = Math.round(score);
+// console.log("mathematics=>", mathematics);
+
+
+// var round= 4.4;
+// var newround = Math.round(round);
+// console.log(newround)
+
+
+// var rounded = 8.44;
+// var rounded1 = Math.round(rounded)   //round off like in maths which studied in primary classes
+// var rounde2 = Math.floor(rounded)    //floor is used for ignore all decimal values , lower like floor
+// var rounded3 = Math.ceil(rounded)   //ceil like roof mean higher , it increase the term if they have any decimal value
+
+// console.log(rounded1)
+// console.log(rounde2)
+// console.log(rounded3)
