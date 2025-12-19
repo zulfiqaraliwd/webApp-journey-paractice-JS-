281f552b9544a290b3ec109a3d834b7428c4cb66
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2025-12-19 22:47:22 +0500

    pre and post increment paractice

--- 6) Maths aperation and round off scene/app.js
+++ 6) Maths aperation and round off scene/app.js
@@ -15,13 +15,43 @@ console.log("round off the numbers in js")
 // var rounded1 = Math.round(rounded)   //round off like in maths which studied in primary classes
 // var rounde2 = Math.floor(rounded)    //floor is used for ignore all decimal values , lower like floor
 // var rounded3 = Math.ceil(rounded)   //ceil like roof mean higher , it increase the term if they have any decimal value
+// var rounded4 = rounded.toFixed(1)   // toFixed is used for to fixed any number 
+
+
+
 
 // console.log(rounded1)
 // console.log(rounde2)
 // console.log(rounded3)
+// console.log(rounded4)
 
 
 //random number in js
 
-var randomNum = Math.random()
-console.log(Math.ceil(randomNum))
+// var randomNum = Math.random()
+// console.log(randomNum)
+
+
+//create a die 
+
+// var die;
+// die = Math.random()*6
+// var dieFix = die.toFixed();
+// if(dieFix==0){
+//     console.log("try again in your next time")
+// }
+// console.log(dieFix)
+
+
+// another thinking
+// var die;
+// die = Math.random()*5+1;
+// var dieFix = die.toFixed();
+// console.log(dieFix);
+
+
+
+//another thinking
+
+var die = Math.floor(Math.random()*6)+1;
+console.log(die);
---
+++ revision of js/app.js
@@ -0,0 +1,9 @@
+console.table("revision of js")
+
+//post and pre-increment in js
+
+var num = 5;
+                console.log(num++ + num + ++num + num++ + num)
+//understanding by visually  5    +  6  +   7   +  7    +  8
+
+
