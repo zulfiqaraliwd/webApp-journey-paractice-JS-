1b349509809ef6504963f14a062422893e0fda3d
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2025-12-20 09:43:24 +0500

    revision paractice

--- revision of js/app.js
+++ revision of js/app.js
@@ -2,14 +2,76 @@ console.table("revision of js")
 
 //post and pre-increment in js
 
-var num = 5;
-                console.log(num++ + num + ++num + num++ + num)
+// var num = 5;
+//                 console.log(num++ + num + ++num + num++ + num)
 //understanding by visually  5    +  6  +   7   +  7    +  8
 
 
-var num = (1 * 2) * (3 + 2);
-console.log(num);
+// var num = (1 * 2) * (3 + 2);
+// console.log(num);
 
 
-var num1 = 1*2+4-5/6;
-console.log(num1)
+// var num1 = 1*2+4-5/6;      //i think that , js follows the bodmas rule by itself very strickly
+// console.log(num1)
+
+
+// Variables
+// vary(change) ables(able)
+// var uname = {
+//   names: "ibrahim",
+//   roll: 30,
+//   age: 18,
+// };
+// var myName = "ibrahim";  //assign
+// var myName ="khan"
+// const lastName = "khan";
+
+// console.warn(myName);
+// console.error(myName);
+// console.table(uname);
+
+// var test;
+// test ="tofy"
+// console.log(test)
+
+// var empty = null
+// // console.log(empty)
+// // console.log(myFirstName)
+// let myFirstName = "ibrahim"  //reserved
+// myFirstName //reinitialized
+// myFirstName = "ibrahim change" //reassign
+
+
+// let cannot access(TDZ = temporal dead zone)
+
+// variable name convention
+// pascal case 
+// camel case (easy to read and write)
+// snake case 
+// train case
+
+
+
+// var arr =[]
+
+// arr[200] = "ali" //length 201 (200 index)
+// arr[150] = "asad" // 151
+// console.log(arr[arr.length-1])
+
+// console.log(arr[arr.length-51])
+// console.log(arr.length)
+
+
+// var arr = []
+// arr[200] = "ali";
+// arr[100] = "zulfiqar ali";
+
+// //i think there are two methods to access array 1) access by index 2) access by length
+
+// //access by index;
+// console.log(arr[200]);
+// console.log(arr[100])
+
+// //accsess by arrY'S  length
+// console.log(arr[arr.length-1])
+// console.log(arr[arr.length-101]) //because we know that "zulfiqar ali " is at index 100 and total length of an array is 
