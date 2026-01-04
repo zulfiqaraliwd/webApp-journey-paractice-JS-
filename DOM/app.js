ad95a163fe536aa8ba9ea60a6e4522e88729247e
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-01-05 00:02:52 +0500

    render of list is completed

--- DOM/app.js
+++ DOM/app.js
@@ -7,16 +7,38 @@ console.log("DOM manipulation")
 // }
 
 
-
-function rollNoS(){
-    var campus = document.getElementById("studentName").value
+// ----------------when you leave the first input than second will filled with the same value of first one-------------------------
+// function rollNoS(){
+//     var campus = document.getElementById("studentName").value
    
-if (campus == "zulfiqar"){
-    console.log("zaitoon ashraf IT park")
-}else{
-    console.log("he is not enroll in any course")
-}
+// if (campus == "zulfiqar"){
+//     console.log("zaitoon ashraf IT park")
+// }else{
+//     console.log("he is not enroll in any course")
+// }
 
-document.getElementById("rollNo").value = campus
+// document.getElementById("rollNo").value = campus
 
-}
+// }
+
+
+
+
+
+// function expandPara(){
+//     var longPara = "our product is very trusty and well checked by special doctors so i am also recommended for gym boys";
+//     var para = document.getElementById("para").innerText = longPara;
+//     console.log("here is your paragraph => ", para)
+// }
+
+// -------------------render list paractice---------------------
+
+// function render(){
+//     var renderList = document.getElementById("render")
+
+//     var list = "<ol><li>ali</li><li>asad</li><li>abdullah</li><li>zulfiqar ali</li></ol>"
+
+
+//     renderList.innerHTML = list;
+    
+// }
