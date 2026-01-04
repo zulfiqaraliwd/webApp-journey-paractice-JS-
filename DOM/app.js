1218dab037b62cb660f8d9aa1ecf0da26b4f3c99
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-01-04 22:12:20 +0500

    dom input paractice

--- DOM/app.js
+++ DOM/app.js
@@ -1,8 +1,22 @@
 console.log("DOM manipulation")
 
 
-var b = 
+// function emailF(){
+//     var email = document.getElementById(email)
+//     console.log("email has been entered")
+// }
 
-function hellow(){
-    console.log("hellow world")
+
+
+function rollNoS(){
+    var campus = document.getElementById("studentName").value
+   
+if (campus == "zulfiqar"){
+    console.log("zaitoon ashraf IT park")
+}else{
+    console.log("he is not enroll in any course")
+}
+
+document.getElementById("rollNo").value = campus
+
 }
