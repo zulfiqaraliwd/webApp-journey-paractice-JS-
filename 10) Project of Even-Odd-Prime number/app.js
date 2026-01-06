11770dae02bd448c03800ab134658081527dbe59
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-01-06 19:26:01 +0500

    counter app is ready

--- 10) Project of Even-Odd-Prime number/app.js
+++ 10) Project of Even-Odd-Prime number/app.js
@@ -1,14 +1,3 @@
-// console.log("numbers")
-
-const { jsx } = require("react/jsx-runtime");
-
-// function increment(){
-// var inc = document.getElementById("inc")
-// var increase = Number(inc.innerHTML);
-// increase++
-// console.log("increase")
-// var inc = document.getElementById("inc").innerHTML=increase;
-// }
 
 function increment(){
     var inc = document.getElementById("incH")
@@ -30,7 +19,8 @@ function decrement(){
 
 
 
-function reset(){
-
-
+function res(){
+ var inc = document.getElementById("incH")
+ var increase = (inc.innerHTML)
+document.getElementById("incH").innerHTML = 0
  }
