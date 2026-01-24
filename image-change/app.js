6c3a4676253c145ef25fdf3b8203cc194fd1b12c
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-01-25 00:10:29 +0500

    mouse over than change the image

--- image-change/app.js
+++ image-change/app.js
@@ -1,5 +1,18 @@
+var changingImage = true;
+
 function change() {
   var image = document.getElementById("changeImage");
-  image.src = "https://thumbs.dreamstime.com/b/negative-sign-grunge-stamp-label-180265477.jpg";
+  if(changingImage){
+    image.src = "https://thumbs.dreamstime.com/b/negative-sign-grunge-stamp-label-180265477.jpg";
+  }else{
+
+    image.src="https://i.ytimg.com/vi/tNE-i9g3ty4/hqdefault.jpg";
+  }
+
+  changingImage = !changingImage
+
 }
+
+
  
+
