b92f3ae8e15964fb9f27ae724c0d3d7e2a40653c
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-01-27 17:16:52 +0500

    paracticing of higher order function

--- 12) Higher Order Functions/app.js
+++ 12) Higher Order Functions/app.js
@@ -1,18 +1,78 @@
 console.log("high order functions")
 
-function test(a,b,callback){
-return callback(a,b)
-}
+// function test(a,b,callback){
+// return callback(a,b)
+// }
+// function sum(q,p){
+// return q+p
+// }
+// function minus(q,p){
+// return q-p
+// }
+// console.log(sum(3,4))
+// console.log(minus(3,4))
 
-function sum(q,p){
-return q+p
 
-}
 
-function minus(q,p){
-return q-p
 
-}
 
-console.log(sum(3,4))
-console.log(minus(3,4))
+// highg order function with arrow function
+
+// function hof(a,b,callback){
+
+//     return callback(a,b)
+
+// }
+
+// sum = (p,q)=>p+q
+
+// sub = (p,q) =>p-q
+
+// multiply = (p,q) =>p*q
+
+// division = (p,q) =>p/q
+
+// console.log(hof(3,4,sum))
+// console.log(hof(3,7,sub))
+
+
+// **************************************************************************
+
+
+// function highOrderFunction(a,b,callback){
+// return callback(a,b)
+
+// }
+// function add(x,y){
+//     return x+y
+// }
+// console.log(highOrderFunction(1,3,add))
+
+
+
+// function hof(a,b,cb){
+//     return cb(a,b)
+// }
+
+// function sum(x,y){
+// return x+y;
+// }
+
+// console.log(hof(4,3,sum))
+
+
+// ************************************************************************************
+
+// function high(a,b,callback){
+// return callback(a,b)
+// }
+
+// function sum(x,y){
+//     return x+y ;
+// }
+
+// result = console.log(high(4,4,sum))
+
+
+
+
