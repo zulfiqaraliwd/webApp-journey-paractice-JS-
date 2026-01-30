a76ea5414851594d99d8162f72935c156e551a4f
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-01-30 11:26:43 +0500

    scoping in js

---
+++ 13)scoping/app.js
@@ -0,0 +1,31 @@
+console.log("js is running");
+
+
+
+// functional scope
+// function test(){
+// var name = "zulfiqar ali"
+
+// }
+// console.log("name is ",name)
+// test()  //it is not accessable.
+
+
+
+//global scope
+// let name = "ali"
+
+// function test(){
+// console.log(name)
+// }
+// test()
+
+
+
+//bloked scope in js
+// if(true){
+//     var name = "abdullah"   //if i write "let" insted of "var" than we cannot access valriable ouside of scope{}. 
+// }
+// console.log(name)
+
+
