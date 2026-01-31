7bcda421e80bafc282b27d1f689685593c48cbd6
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-01-31 18:06:43 +0500

    node in js

---
+++ 9)Nodes/app.js
@@ -0,0 +1,24 @@
+// var para = document.createElement('p')
+// let text = document.createTextNode('hi')
+// para.appendChild(text)
+// document.body.appendChild(para)
+
+
+
+// var heading = document.createElement('h1')
+// let text = document.createTextNode('this is js')
+// let h = heading.appendChild(text)
+// document.body.appendChild(heading)
+
+
+
+// var bold = document.createElement('b')
+// var text = document.createTextNode('this is bold')
+// bold.appendChild(text)
+// var res1 = document.body.appendChild(bold)
+
+
+// var h2 = document.createElement('h2');
+// var text = document.createTextNode("this is h2");
+// h2.appendChild(text)
+// var res = document.body.appendChild(h2)
