cf921bf305b4852bf7b5e586a4b23a04c1968724
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-02-09 00:49:18 +0500

    todo app

--- todo app/app.js
+++ todo app/app.js
@@ -1,6 +1,15 @@
 var input = document.getElementById("input")
 var btn = document.getElementById("btn")
+var show = document.getElementById("ul")
 
 btn.addEventListener('click', ()=>{
-var render = document.getElementById('show').innerHTML += `<li>${input.value}</li>`
+var li = document.createElement("li")
+var text = document.createTextNode(input.value)
+li.appendChild(text)
+var res = document.body.appendChild(li)
+
+input.value  = "";
+
 })
+
+
