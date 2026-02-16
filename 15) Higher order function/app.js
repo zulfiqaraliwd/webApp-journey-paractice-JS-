610c7f5d730ec67fcf8dacb7d3251095f77cdcf2
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-02-16 11:29:20 +0500

    higher order func

---
+++ 15) Higher order function/app.js
@@ -0,0 +1,27 @@
+// console.log("higer order function")
+
+
+// function high(a,b,callback){
+//     return callback(a,b);
+// }
+
+// function sum(c,d){
+//     return c+d;
+// }
+
+// console.log(sum(2,4))
+
+
+// function high(a,b,callback){
+//     return callback(a,b)
+// }
+
+// function sum(c,d){
+//     return c+d;
+// }
+// function multiply(a,b){
+//     return a*b
+// }
+
+// console.log(high(2,4,sum))
+// console.log(high(3,4,multiply))
