d866f9d58c9b6cb120d1aa785fd6471813bddfeb
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-02-19 09:57:50 +0500

    paractice start in ramadan

---
+++ filter app demo paractice/app.js
@@ -0,0 +1,25 @@
+var arr = [
+    {title : "Apricodode",
+        id: 1,
+        price : 1000,
+        taste: "Sweet"
+    },
+     {title : "Almont",
+        id: 2,
+        price : 1200,
+        taste: "Nutty"
+    }
+    ,
+      {title : "Walnuts",
+        id: 3,
+        price : 1800,
+        taste: "Bitter"
+    },
+      {title : "shehtoot",
+        id: 4,
+        price : 1800,
+        taste: "Sweet"
+    }
+]
+
+
